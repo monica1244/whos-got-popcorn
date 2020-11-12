c8158f26237ad681826e1cb26c39732fc5a7c0be
Monica Gupta <monica.gupta.ms@gmail.com>
2020-11-12 14:10:08 -0500

    Added bubble chart

--- test.js
+++ test.js
@@ -1,10 +1,1 @@
 console.log("Enter");
-function ChangeY(y_axis){
-  console.log(y_axis);
-}
-function AddGenre(genre){
-  console.log(genre);
-}
-function RemoveGenre(genre){
-  console.log(genre);
-}
