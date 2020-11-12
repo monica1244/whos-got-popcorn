1fad2bbfb0e8e6340796aa92d1917fd670684264
Monica Gupta <monica.gupta.ms@gmail.com>
2020-11-12 04:00:09 -0500

    Added json

--- bubble.js
+++ bubble.js
@@ -12,7 +12,7 @@ var pack = d3.pack()
     .size([diameter - margin, diameter - margin])
     .padding(2);
 
-d3.json("data_fin.json", function(error, root) {
+d3.json("data.json", function(error, root) {
   if (error) throw error;
 
   root = d3.hierarchy(root)
